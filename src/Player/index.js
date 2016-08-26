5056461a65eb977a47bb3bbceaf156fae202e791
Benjamin Devaublanc <bdevaublanc@molotov.tv>
2016-08-26 16:54:26 +0200

    refactoring

--- src/Player/index.js
+++ src/Player/index.js
@@ -1,34 +1,22 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './index.css';
 
+/* global MediaPlayer, Dash, cast */
 const PROTOCOL = "urn:x-cast:org.dashif.dashjs";
-const CHANNEL = "org.dashif.dashjs.channel";
 
 class Player extends Component {
 
-    init = (messageBus, mediaManager) => {
-        console.log('init', messageBus, mediaManager);
-        console.log("init====");
-        this.messageBus = messageBus;
-        this.messageBus.addEventListener(cast.receiver.CastMessageBus.EventType.MESSAGE, this.onDashMessage);
+    player = null
+    video = null
+    receiver = null
+    mediaManeger = null
+    messageBus = null
 
-        this.mediaManager = mediaManager;
-        this.mediaManager.setMediaElement(this.video);
-        this.mediaManager.onLoad = this.onLoad;
-
-        this.player = new MediaPlayer(new Dash.di.DashContext());
-
-        this.player.startup();
-        this.player.setAutoPlay(true);
-        this.player.attachView(this.video);
-    }
-
-    startVideo = (newMedia) => {
+    loadStream = (newMedia) => {
         this.media = newMedia;
 
         console.log("this.media", this.media);
-        var url, assetId, token, urlParams;
+        let url, assetId, token;
 
         // url = media.contentId;
         // assetId = media.customData.drmParams.assetId;
@@ -37,9 +25,9 @@ class Player extends Component {
 
         url = 'http://v.cdn3.live.mlflux.net/public/france3/france3.isml/france3.mpd';
         assetId = 'france3';
-        token = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJpYXQiOjE0NzIyMTc3NDMsImp0aSI6IjU3YzA0MjhmZDYwMjEiLCJjcnQiOiJbe1wiYXNzZXRJZFwiOlwiZnJhbmNlM1wiLFwic3RvcmVMaWNlbnNlXCI6ZmFsc2UsXCJwcm9maWxlXCI6e1wicmVudGFsXCI6e1wiYWJzb2x1dGVFeHBpcmF0aW9uXCI6XCIyMDE2LTA4LTI2VDE5OjIyOjIzKzAyOjAwXCIsXCJwbGF5RHVyYXRpb25cIjoxNDQwMDAwMH19LFwibWVzc2FnZVwiOlwiTGljZW5zZSBncmFudGVkXCIsXCJvdXRwdXRQcm90ZWN0aW9uXCI6e1wiZGlnaXRhbFwiOnRydWUsXCJhbmFsb2d1ZVwiOnRydWUsXCJlbmZvcmNlXCI6ZmFsc2V9fV0iLCJvcHREYXRhIjoie1widXNlcklkXCI6XCI4ZjNhYjAwNWEzN2Y0N2U5MDNhYWJmYTFjOWFlMTRlNDdkZTQxM2I3XCIsXCJzZXNzaW9uSWRcIjpcImF2MDQ1M29ia2ZiMW5ubXE0aXNnXCIsXCJtZXJjaGFudFwiOlwibW9sb3RvdlwifSJ9._s_yFUIH2CWFXaW3n_05ulc2FgQEeKqIQPAL5q4p--JK4xmyvP3uISVI6NNCVfyHF8ZkzAcFmdcSXBucnIb9lA';
+        token = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJpYXQiOjE0NzIyMjMxMjksImp0aSI6IjU3YzA1Nzk5Mjg5ODQiLCJjcnQiOiJbe1wiYXNzZXRJZFwiOlwiZnJhbmNlM1wiLFwic3RvcmVMaWNlbnNlXCI6ZmFsc2UsXCJwcm9maWxlXCI6e1wicmVudGFsXCI6e1wiYWJzb2x1dGVFeHBpcmF0aW9uXCI6XCIyMDE2LTA4LTI2VDIwOjUyOjA5KzAyOjAwXCIsXCJwbGF5RHVyYXRpb25cIjoxNDQwMDAwMH19LFwibWVzc2FnZVwiOlwiTGljZW5zZSBncmFudGVkXCIsXCJvdXRwdXRQcm90ZWN0aW9uXCI6e1wiZGlnaXRhbFwiOnRydWUsXCJhbmFsb2d1ZVwiOnRydWUsXCJlbmZvcmNlXCI6ZmFsc2V9fV0iLCJvcHREYXRhIjoie1widXNlcklkXCI6XCI4ZjNhYjAwNWEzN2Y0N2U5MDNhYWJmYTFjOWFlMTRlNDdkZTQxM2I3XCIsXCJzZXNzaW9uSWRcIjpcImF2MDVmNG9ia2ZiMW5ubXE0bmxnXCIsXCJtZXJjaGFudFwiOlwibW9sb3RvdlwifSJ9.npP-tnRutAA9qxgj3Wzk1TlIy8nLE5nsom4T-mltmlCy_3dSfyqUW7TnMta9yaTaRJs-mkSVTru1QOrsh-BZ3A';
 
-        var dataVideo = {
+        const dataVideo = {
             name: assetId,
             url: url,
             browsers: '',
@@ -54,30 +42,13 @@ class Player extends Component {
             }
         }
 
-
-        console.log("dataVideo.protData", dataVideo.protData);
-
-
-        // this.video.addEventListener('stalled', onVideoBuffering.bind(this), false);
-        // this.video.addEventListener('waiting', onVideoBuffering.bind(this), false);
-        // this.video.addEventListener('playing', onVideoPlaying.bind(this),  false);
-
         this.player.attachSource(dataVideo.url, null, dataVideo.protData);
 
-        // $scope.showSplashScreen = false;
-        // $scope.showSpinner = true;
-        // $scope.showVideo = true;
-        // $scope.showStats = false;
-        //
-        // // setTimeout(update, graphUpdateInterval);
-        //
-        // $scope.$apply();
     }
-
 
     onLoad = (e) => {
         console.log('onLoad====');
-        this.startVideo(e.data.media);
+        this.loadStream(e.data.media);
 
         // this.video.addEventListener("timeupdate", onVideoTime.bind(this));
         // this.video.addEventListener("durationchange", onVideoDuration.bind(this));
@@ -90,43 +61,52 @@ class Player extends Component {
     }
 
     componentDidMount() {
-        console.log('componentDidMount');
-
 
         this.video = this.refs['video'];
 
 
-        const receiver = cast.receiver.CastReceiverManager.getInstance();
+        /////////// INIT CAST ///////////
+
+        // init receiver
+        this.receiver = cast.receiver.CastReceiverManager.getInstance();
         const appConfig = new cast.receiver.CastReceiverManager.Config();
         appConfig.statusText = "DashJS Molotov Receiver";
+        this.messageBus = this.receiver.getCastMessageBus(PROTOCOL);
+
+        // attach mediaManager
+        this.mediaManager = new cast.receiver.MediaManager(this.video);
+        this.mediaManager.setMediaElement(this.video);
+        this.mediaManager.onLoad = this.onLoad;
 
 
-        const messageBus = receiver.getCastMessageBus(PROTOCOL);
-        const mediaManager = new cast.receiver.MediaManager(this.video);
+        ///////// INIT DASH.JS /////////
+
+        // init player
+        this.player = new MediaPlayer(new Dash.di.DashContext());
+        this.player.startup();
+        this.player.setAutoPlay(true);
+        this.player.attachView(this.video);
 
 
-        this.init(messageBus, mediaManager);
+        ///////// BIND EVENTS /////////
 
-        receiver.onSenderDisconnected = function(event) {
-            if ((receiver.getSenders().length === 0) && (event.reason === cast.receiver.system.DisconnectReason.REQUESTED_BY_SENDER)) {
+        // Cast events
+        this.receiver.onSenderDisconnected = (event) => {
+            if ((this.receiver.getSenders().length === 0)
+            && (event.reason === cast.receiver.system.DisconnectReason.REQUESTED_BY_SENDER)) {
                 window.close();
             }
         };
 
-        receiver.start(appConfig);
+        // Video events
+
+        this.receiver.start(appConfig);
     }
 
     render() {
         return (
-            <div className="App">
-                <div className="App-header">
-                    <img src={logo} className="App-logo" alt="logo" />
-                    <h2>Chromecast Receiver React</h2>
-                </div>
-                <p className="App-intro">
-                    To get started, edit <code>src/App.js</code> and save to reload.
-                </p>
-                <video ref="video" autoPlay></video>
+            <div className="Player">
+                <video ref="video" autoPlay className="Player-video"></video>
             </div>
         );
     }
