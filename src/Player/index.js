a56247a7b756d1b607f425dd4838b766aa61425a
Benjamin <benjamin.devaublanc@gmail.com>
2016-09-08 15:10:26 +0200

    feat(loader): create loader, fix splash screen (#1)

--- src/Player/index.js
+++ src/Player/index.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
+
+import Splash from '../Splash';
+import Loader from '../Loader';
+
 import './index.css';
-import Logo from '../Logo';
 
 /* global dashjs, cast */
 const PROTOCOL = "urn:x-cast:org.dashif.dashjs";
@@ -13,11 +16,52 @@ class Player extends Component {
     receiver = null
     mediaManeger = null
     messageBus = null
+
     state = {
-        splash: true
+        splash: true,
+        loading: false
     };
 
-    getProtectionConfig = (token) =>{
+
+    /**
+     * Load the stream
+     * @param  {object} media cast
+     * @return {undefined}
+     */
+    loadFlux = ({ contentId, metadata }) => {
+        const protectionConfig = this.getProtectionConfig(metadata.asset.token);
+        this.loader(true);
+        this.video.setMediaKeys(null).then(() => {
+            this.player.setProtectionData(protectionConfig);
+            this.player.attachSource(contentId);
+            this.player.play();
+        });
+    }
+
+    /**
+     * Set the loader status
+     * @param  {bool} status of loader
+     * @return {undefined}
+     */
+    loader = (status) => {
+        this.setState({
+            loading: status
+        });
+    }
+
+    /************************************************************************************************************************************************
+    /************************************************************************************************************************************************
+    ***************************************************************** GETTERS ***********************************************************************
+    /************************************************************************************************************************************************
+    /************************************************************************************************************************************************/
+
+
+    /**
+    * Return protection object mandatory for DRM stream
+    * @param {string} token drm token
+    * @return {object} protection
+    */
+    getProtectionConfig = (token) => {
         return {
             'com.widevine.alpha': {
                 drmtoday: true,
@@ -26,37 +70,79 @@ class Player extends Component {
                     'x-dt-auth-token': token
                 }
             }
-        };
+        }
     }
 
-    loadStream = ({ contentId, metadata }) => {
-        const protectionConfig = this.getProtectionConfig(metadata.asset.token);
+    /************************************************************************************************************************************************
+    /************************************************************************************************************************************************
+    ************************************************************* HANDLE EVENTS ********************************************************************
+    /************************************************************************************************************************************************
+    /************************************************************************************************************************************************/
 
-        this.video.setMediaKeys(null).then(() => {
-            console.log('============================= set protection and play');
-            this.player.setProtectionData(protectionConfig);
-            this.player.attachSource(contentId);
-            console.log('=============================this.player.play();');
-            this.setState({splash: false});
-            this.player.play();
+    /************************************************************/
+    /********************** Video EVENTS ************************/
+    /************************************************************/
+
+    /**
+     * On player dash fire event STREAM_INITIALIZED
+     * @return {undefined}
+     */
+    handleStreamInitialized = () => {
+        this.setState({
+            splash: false
         });
-
-
-
     }
 
-    onLoad = (e) => {
-        console.log('onLoad++++++++++++++++++++++++++++', e);
-        this.loadStream(e.data.media);
-
-        // this.video.addEventListener("timeupdate", onVideoTime.bind(this));
-        // this.video.addEventListener("durationchange", onVideoDuration.bind(this));
-        // this.video.addEventListener("ended", onVideoEnded.bind(this));
+    handleCanplay = () => {
+        this.loader(false);
     }
 
+    /**
+     * On player dash fire event BUFFER_EMPTY
+     * @return {undefined}
+     */
+    handleBufferEmpty = () => {
+        this.loader(true);
+    }
+
+    /**
+     * On player dash fire event BUFFER_LOADED
+     * @return {undefined}
+     */
+    handleBufferLoaded = () => {
+        this.loader(false);
+    }
+
+    /************************************************************/
+    /********************** MediaManager EVENTS *****************/
+    /************************************************************/
+
+    /**
+     * On Media manager receive a media
+     * @return {undefined}
+     */
+    onMediaManagerLoad = (e) => {
+        this.loadFlux(e.data.media);
+    }
+
+
+    /**
+     * On Media manager receive a message
+     * @return {undefined}
+     */
     onDashMessage = (e) => {
-        var message = e.message;
+        const message = e.message;
         console.debug('Message received', JSON.stringify(message));
+    }
+
+    /************************************************************************************************************************************************
+    /************************************************************************************************************************************************
+    ********************************************************** COMPONENT LIFECYCLE ******************************************************************
+    /************************************************************************************************************************************************
+    /************************************************************************************************************************************************/
+
+    shouldComponentUpdate(nextProps, nextState) {
+        return !nextState.splash
     }
 
     componentDidMount() {
@@ -75,7 +161,7 @@ class Player extends Component {
         // attach mediaManager
         this.mediaManager = new cast.receiver.MediaManager(this.video);
         this.mediaManager.setMediaElement(this.video);
-        this.mediaManager.onLoad = this.onLoad;
+        this.mediaManager.onLoad = this.onMediaManagerLoad;
 
 
         ///////// INIT DASH.JS /////////
@@ -88,6 +174,25 @@ class Player extends Component {
 
 
         ///////// BIND EVENTS /////////
+        this.player.on(
+            dashjs.MediaPlayer.events.CAN_PLAY,
+            this.handleCanplay
+        );
+
+        this.player.on(
+            dashjs.MediaPlayer.events.STREAM_INITIALIZED,
+            this.handleStreamInitialized
+        );
+
+        this.player.on(
+            dashjs.MediaPlayer.events.BUFFER_EMPTY,
+            this.handleBufferEmpty
+        );
+
+        this.player.on(
+            dashjs.MediaPlayer.events.BUFFER_LOADED,
+            this.handleBufferLoaded
+        );
 
         // Cast events
         this.receiver.onSenderDisconnected = (event) => {
@@ -102,9 +207,17 @@ class Player extends Component {
     }
 
     render() {
+
+        const {
+            loading,
+            splash
+        } = this.state;
+
+
         return (
             <div className="Player" ref="container">
-                { this.state.splash && <Logo /> }
+                { splash && <Splash /> }
+                { loading && <Loader /> }
                 <video ref="video" autoPlay="false" className="Player-video"></video>
             </div>
         );
